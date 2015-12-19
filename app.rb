af00de6644b605f1daeb869c39784ea48c55a56b
LSChyi <alan81920@gmail.com>
2015-12-19 22:54:11 +0800

    update server

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 require 'mysql2'
+require 'json'
 
 set :bind, '0.0.0.0'
 
 get '/' do
     send_file 'public/index.html'
+end
+
+post '/message' do
+    received = JSON.parse(request.body.read)    
+    {
+        :state => "Success"
+    }.to_json
 end
--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@ angular.module('myApp', [])
         }
 
         $scope.send = function() {
+            if($scope.name == "" || !$scope.name) {
+                $scope.name = "匿名";
+            }
             if($scope.content == "" || !$scope.content) {
                 $.growl.error({ 
                     title: "",
@@ -15,13 +18,25 @@ angular.module('myApp', [])
                 return;
             }
             else {
-                $.growl.notice({
-                    title: "",
-                    message: "存進server囉~松錡感謝你 <3"
+                $http.post("/message", {
+                    name: $scope.name,
+                    content: $scope.content
                 })
-                $scope.content = "";
-                $scope.name = "";
-                $('#name').focus();
+                    .success(function() {
+                        $.growl.notice({
+                            title: "",
+                            message: "存進server囉~松錡感謝你 <3"
+                        })
+                        $scope.content = "";
+                        $scope.name = "";
+                        $('#name').focus();
+                    })
+                    .error(function() {
+                        $.growl.error({
+                            title: "",
+                            message: "糟糕，server好像怪怪的。先去逛逛再來留言吧~(或者呼叫松錡來修QQ)"
+                        })
+                    })
             }
         }
     })
