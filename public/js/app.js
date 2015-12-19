56a062649dba92ecca4748963040d9ce182ab774
LSChyi <alan81920@gmail.com>
2015-12-19 22:41:36 +0800

    add interaction

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 angular.module('myApp', [])
-    .controller('mainCtrl', function($scope) {
+    .controller('mainCtrl', function($scope, $http) {
         $scope.init = function() {
             angular.element(document).ready(function() {
                 $('#name').focus();
@@ -7,6 +7,21 @@ angular.module('myApp', [])
         }
 
         $scope.send = function() {
-            
+            if($scope.content == "" || !$scope.content) {
+                $.growl.error({ 
+                    title: "",
+                    message: "你沒填留言喔 =3="
+                })
+                return;
+            }
+            else {
+                $.growl.notice({
+                    title: "",
+                    message: "存進server囉~松錡感謝你 <3"
+                })
+                $scope.content = "";
+                $scope.name = "";
+                $('#name').focus();
+            }
         }
     })
